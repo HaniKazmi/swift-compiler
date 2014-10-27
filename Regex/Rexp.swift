2101680016706b2639603cd40d26240b68f30ffa
Hani Kazmi <hanikazmi@me.com>
2014-10-27 17:17:15 +0000

    Working Rexp Matcher

--- Regex/Rexp.swift
+++ Regex/Rexp.swift
@@ -6,20 +6,66 @@
 //  Copyright (c) 2014 Hani. All rights reserved.
 //
 
-protocol Rexp {}
+protocol Rexp: Printable {}
+class UnaryRexp: Rexp { let r: Rexp; init(_ r: Rexp) { self.r = r } ; var description: String { return "" } }
+class BinaryRexp: Rexp { let r1: Rexp, r2: Rexp; init(_ r1: Rexp, _ r2: Rexp) { self.r1 = r1; self.r2 = r2 } ; var description: String { return "" } }
 
-struct Null: Rexp {}
-struct Empty: Rexp {}
-struct Char: Rexp { let c: Character; init(_ c: Character) { self.c = c } }
-struct Alt: Rexp { let r1: Rexp, r2: Rexp; init(_ r1: Rexp, _ r2: Rexp) { self.r1 = r1; self.r2 = r2 } }
-struct Seq: Rexp { let r1: Rexp, r2: Rexp; init(_ r1: Rexp, _ r2: Rexp) { self.r1 = r1; self.r2 = r2 } }
-struct Star: Rexp { let r: Rexp; init(_ r: Rexp) { self.r = r } }
+class Null: Rexp { var description: String { return "Null" } }
+class Empty: Rexp {var description: String { return "Empty" } }
+extension String: Rexp { public var description: String { return "\(self)" } }
+class Alt: BinaryRexp { override var description: String { return "Alt(\(self.r1), \(self.r2))" } }
+class Seq: BinaryRexp { override var description: String { return "Seq(\(self.r1), \(self.r2))" } }
+class Star: UnaryRexp { override var description: String { return "Star(\(self.r))" } }
 
-struct Range: Rexp { let c: [Character]; init(_ c: String) { self.c = Array(c) } }
-struct Plus: Rexp { let r: Rexp; init(_ r: Rexp) { self.r = r } }
-struct Opt: Rexp { let r: Rexp; init(_ r: Rexp) { self.r = r } }
-struct Multiple: Rexp { let r: Rexp, n: Int, m: Int }
-struct Not: Rexp { let r: Rexp; init(_ r: Rexp) { self.r = r } }
+class Range: Rexp { let c: [Character]; init(_ c: String) { self.c = Array(c) } ; var description: String { return "Range" } }
+class Plus: UnaryRexp { override var description: String { return "Plus(\(self.r))" } }
+class Opt: UnaryRexp { override var description: String { return "Opt(\(self.r))" } }
+class Mult: Rexp { let r: Rexp, n: Int, m: Int; init(r: Rexp, n: Int, m: Int) {self.r = r; self.n = n; self.m = m} ; var description: String { return "Mult(\(self.r), \(self.n), \(self.m))" } }
+class Not: UnaryRexp { override var description: String { return "Not(\(self.r))" } }
+
+func nullable(r: Rexp) -> Bool {
+    switch r {
+    case is Null: return false
+    case is Empty: return true
+    case is String: return false
+    case let r as Alt: return nullable(r.r1) || nullable(r.r2)
+    case let r as Seq: return nullable(r.r1) && nullable(r.r2)
+    case is Star: return true
+    case is Range: return false
+    case let r as Plus: return nullable(r.r)
+    case let r as Opt: return true
+    case let r as Mult: return r.n <= 0 ? true : nullable(r.r)
+    case let r as Not: return !nullable(r.r)
+    default: return false
+    }
+}
+
+func der(c: Character, r: Rexp) -> Rexp {
+    switch r {
+    case is Null: return Null()
+    case is Empty: return Null()
+    case let r as String: return String(c) == r ? Empty() : Null()
+    case let r as Alt: return Alt(der(c, r.r1), der(c, r.r2))
+    case let r as Seq: return nullable(r.r1) ?
+        Alt(Seq(der(c, r.r1), r.r2), der(c, r.r2)) : Seq(der(c, r.r1), r.r2)
+    case let r as Star: return Seq(der(c, r.r), Star(r.r))
+    case let r as Range: return contains(r.c, c) ? Empty() : Null()
+    case let r as Plus: return Seq(der(c, r.r), Star(r.r))
+    case let r as Opt: return der(c, r.r)
+    case let r as Mult: return r.m == 0 ? Null() : Seq(der(c, r.r), Mult(r: r.r, n: r.n-1, m: r.m-1))
+    case let r as Not: return Not(der(c, r.r))
+    default: return r
+    }
+}
+
+func ders(s: [Character], r:Rexp) -> Rexp {
+    if s.isEmpty { return r }
+    return ders(Array(s[1..<s.count]), simp(der(s[0], r)))
+}
+
+func matches(r: Rexp, s:String) -> Bool {
+    return nullable(ders(Array(s), r))
+}
 
 func simp(r: Rexp) -> Rexp {
     switch r {
@@ -27,6 +73,7 @@ func simp(r: Rexp) -> Rexp {
         switch (simp(r.r1), simp(r.r2)) {
         case (is Null, let r2): return r2
         case (let r1, is Null): return r1
+        case let (r1, r2) where r1==r2: return r1
         case let (r1, r2): return Alt(r1, r2)
         }
         
@@ -39,67 +86,45 @@ func simp(r: Rexp) -> Rexp {
         case let (r1, r2): return Seq(r1, r2)
         }
         
+    case let r as Mult: return Mult(r: simp(r.r), n: r.n, m: r.m)
+    case let r as Plus: return Plus(simp(r.r))
+    case let r as Star: return Star(simp(r.r))
     default: return r
     }
 }
 
-func + (left: Rexp, right: Rexp) -> Rexp {
-    return Alt(left, right)
+func count(r: Rexp) -> Int {
+    switch r{
+    case let r as BinaryRexp: return 1 + count(r.r1) + count(r.r2)
+    case let r as UnaryRexp: return 1 + count(r.r)
+    case let r as Mult: return 1 + count(r.r)
+    default: return 1
+    }
 }
 
-func & (left: Rexp, right: Rexp) -> Rexp {
-    return Seq(left, right)
-}
-
-postfix operator * {}
-postfix func * (star: Rexp) -> Rexp {
-    return Star(star)
-}
-
-prefix func ! (r: Rexp) -> Rexp {
-    return Not(r)
-}
-
-func nullable(r: Rexp) -> Bool {
-    switch r {
-    case is Null: return false
-    case is Empty: return true
-    case is Char: return false
-    case let r as Alt: return nullable(r.r1) || nullable(r.r2)
-    case let r as Seq: return nullable(r.r1) && nullable(r.r2)
-    case is Star: return true
-    case is Range: return false
-    case let r as Plus: return nullable(r.r)
-    case let r as Opt: return true
-    case let r as Multiple: return r.n <= 0 ? true : false
-    case let r as Not: return !nullable(r.r)
+func ==(r1: Rexp, r2: Rexp) -> Bool {
+    switch (r1, r2) {
+    case is (Null, Null): return true
+    case is (Empty, Empty): return true
+    case let (r1 as String, r2 as String): return r1.description == r2.description
+    case let (r1 as Alt, r2 as Alt): return r1.r1 == r2.r1 && r1.r2 == r2.r2
+    case let (r1 as Seq, r2 as Seq): return r1.r1 == r2.r1 && r1.r2 == r2.r2
+    case let (r1 as Opt, r2 as Opt): return r1.r == r2.r
+    case let (r1 as Star, r2 as Star): return r1.r == r2.r
+    case let (r1 as Plus, r2 as Plus): return r1.r == r2.r
     default: return false
     }
 }
 
-func der(c: Character, r: Rexp) -> Rexp {
-    switch r {
-    case is Null: return Null()
-    case is Empty: return Null()
-    case let r as Char: return c == r.c ? Empty() : Null()
-    case let r as Alt: return Alt(der(c, r.r1), der(c, r.r2))
-    case let r as Seq: return nullable(r.r1) ?
-        Alt(Seq(der(c, r.r1), r.r2), der(c, r.r2)) : Seq(der(c, r.r1), r.r2)
-    case let r as Star: return Seq(der(c, r.r), Star(r.r))
-    case let r as Range: return contains(r.c, c) ? Empty() : Null()
-    case let r as Plus: return Seq(der(c, r.r), Star(r.r))
-    case let r as Opt: return der(c, r.r)
-    case let r as Multiple: return r.m == 0 ? Null() : Seq(der(c, r.r), Multiple(r: r.r, n: r.n-1, m: r.m-1))
-    case let r as Not: return Not(der(c, r.r))
-    default: return r
-    }
-}
+func |(r1: Rexp, r2: Rexp) -> Rexp { return Alt(r1, r2) }
+func &(r1: Rexp, r2: Rexp) -> Rexp { return Seq(r1, r2) }
+func ^(r: Rexp, p:[Int]) -> Rexp { return Mult(r: r, n: p[0], m: p[1])}
 
-func ders(s: [Character], r:Rexp) -> Rexp {
-    if s.isEmpty { return r }
-    return ders(Array(s[1..<s.count]), der(s[0], r))
-}
+prefix func !(r: Rexp) -> Rexp { return Not(r) }
 
-func matches(r: Rexp, s:String) -> Bool {
-    return nullable(ders(Array(s), r))
-}
+postfix operator * {}
+postfix func *(r: Rexp) -> Rexp { return Star(r) }
+postfix operator + {}
+postfix func +(r: Rexp) -> Rexp { return Plus(r) }
+postfix operator % {}
+postfix func %(r: Rexp) -> Rexp { return Opt(r) }
--- Regex/main.swift
+++ Regex/main.swift
@@ -8,6 +8,45 @@
 
 import Foundation
 
-let r = Char("/") & Char("*") & !(Range("abcdefghijklmnopqrstuvqxyz")* & Char("*") & Char("/") & Range("abcdefghijklmnopqrstuvqxyz")*) & Char("*") & Char("/")
-let x = matches(r, "/*test*/test*/")
-println(x)
+// Helper functions
+func printR(r: Rexp, s: String) {
+    println("\(s): \(matches(r, s))")
+}
+
+infix operator ^ { precedence 160 }
+
+let allLetters = "abcdefghijklmnopqrstuvwxyz"
+let allNums = "0123456789"
+let allSym = "_.-€"
+let allChars = allLetters + allNums + allSym
+
+
+// Question 3
+let e = Range(allChars)+ & "@" & Range(allLetters+"."+"-")+ & "." & (Range(allLetters+".")^[2,6])
+printR(e, "hani.kazmi@kcl.ac.uk")
+println(ders(Array("hani.kazmi@kcl.ac.uk"), e))
+
+
+// Question 4
+let r = "/" & "*" & !(Range("abcdefghijklmnopqrstuvqxyz")* & "*" & "/" & Range("abcdefghijklmnopqrstuvqxyz")*) & "*" & "/"
+
+printR(r, "/**/")
+printR(r, "/*foobar*/")
+printR(r, "/*test*/test*/")
+printR(r, "/*test*/*test*/")
+
+// Question 5
+let r1 = "a" & "a" & "a"
+let r2 = "a"^[19,19] & "a"%
+
+let a1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+let a2 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+let a3 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+printR((r1+)+, a1)
+printR((r1+)+, a2)
+printR((r1+)+, a3)
+
+printR((r2+)+, a1)
+printR((r2+)+, a2)
+printR((r2+)+, a3)
